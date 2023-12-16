c7e2bd9275b59e4a4ec58c95443dba2014932165
boyani-Otieno Sylvia <boyani.ombogo7@gmail.com>
2023-12-16 14:23:44 +0000

    Added Paypal payment gateway

--- src/routes/Support/Donate.js
+++ src/routes/Support/Donate.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+//import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import PayPalPayment from './PayPalPayment'
 
 export default function Donate() {
 
-  const initialOptions = {
-    clientId: "AavAxwjTI5fllRVD5q5LX4owxx8WNX9y0BvoVkawwTa_fZPX6i5omRaiqUc9r3tlqtA4JFo-uCTDpaeU",
+  const initialOptions = { 
+    clientId: "Ae-vBbWIdIibTDbDXlSPaR1sJFlvNem_OkwB0HtEqn5TDBbnqWRK9Nr__ph70dXz3uL8rFNMdiHLPiXR",
     currency: "USD",
     intent: "capture",
 };
@@ -13,8 +15,8 @@ export default function Donate() {
     <PayPalScriptProvider options={initialOptions}>
       <div>
         <h1>Support Our Ministry</h1>
-        <button>Donate</button>
-        <PayPalButtons />
+        {/* <button>Donate</button> */}
+        <PayPalPayment />
       </div>
     </PayPalScriptProvider>
   )
