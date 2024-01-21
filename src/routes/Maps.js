bff9b03c292d3ea54eab34bdb9e8f66baf831d11
boyani-Otieno Sylvia <boyani.ombogo7@gmail.com>
2024-01-21 13:36:11 +0000

    edit maps header

--- src/routes/Maps.js
+++ src/routes/Maps.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Maps() {
   return (
     <div>
-        <h1>Our Locations</h1>
+        <h2>Our Locations</h2>
         <iframe src="https://www.google.com/maps/d/embed?mid=1vlP065ps4WU7MvAbnllDqNBU2IIu94o&ehbc=2E312F" 
         width="1500" height="480"></iframe>
     </div>
