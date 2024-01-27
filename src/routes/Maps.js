67f1a5d215bafc8543f24bae60b53e1a2c274e2e
boyani-Otieno Sylvia <boyani.ombogo7@gmail.com>
2024-01-27 16:09:18 +0000

    edit maps for smaller screens

--- src/routes/Maps.js
+++ src/routes/Maps.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Maps() {
   return (
-    <div>
+    <div className='maps'>
         <h2>Our Locations</h2>
-        <iframe src="https://www.google.com/maps/d/embed?mid=1vlP065ps4WU7MvAbnllDqNBU2IIu94o&ehbc=2E312F" 
+        <iframe className="maps" title="Maps" src="https://www.google.com/maps/d/embed?mid=1vlP065ps4WU7MvAbnllDqNBU2IIu94o&ehbc=2E312F" 
         width="1200" height="480"></iframe>
     </div>
   )
